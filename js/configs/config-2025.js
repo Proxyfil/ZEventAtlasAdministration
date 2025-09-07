cc8cf88e436c7199f3e25eb4d09cb8de11616a73
Proxyfil <piloupilou200@gmail.com>
2025-09-08 00:45:46 +0200

    feat: Updated for ZEvent2025

---
+++ js/configs/config-2025.js
@@ -0,0 +1,14 @@
+const canvasSize = {
+	x: 1000,
+	y: 1000
+}
+window.canvasSize = canvasSize
+
+const canvasCenter = {
+	x: canvasSize.x/2,
+	y: canvasSize.y/2
+}
+window.canvasCenter = canvasCenter
+
+let variationsConfig = {}
+window.variationsConfig = variationsConfig
